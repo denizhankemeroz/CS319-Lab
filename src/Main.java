38682954b92db5e93c18d74524f14380a6577396
Denizhan Kemeröz <denizhan.kemeroz@ug.bilkent.edu.tr>
2020-10-24 13:55:08 +0300

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        PrimeGenerator primeGenerator = new PrimeGenerator();
+        6
+        System.out.println(primeGenerator.generatePrimes(50));
     }
+
 }
